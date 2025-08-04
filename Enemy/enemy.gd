544fb8ba5ea55f4e146da38b52ea09d0a0bdd02e
Sam-xiexielaoshi <samagrasingh20032003@gmail.com>
2025-08-05 01:27:14 +0530

    added aniumation and have to resolve the issue of not being able to connect the singal fropm hurt_box to the enemy gd

--- Enemy/enemy.gd
+++ Enemy/enemy.gd
@@ -2,10 +2,21 @@ extends CharacterBody2D
 
 
 @export var movement_speed = 20.0
+@export var hp = 10
+@onready var sprite: Sprite2D = $Sprite2D
+@onready var anim: AnimationPlayer = $AnimationPlayer
 
 @onready var player = get_tree().get_first_node_in_group("player")
+
+func _ready() -> void:
+	anim.play("walk")
 
 func _physics_process(_delta: float) -> void:
 	var direction = global_position.direction_to(player.global_position)
 	velocity = direction*movement_speed
 	move_and_slide()
+	
+	if direction.x > 0.1:
+		sprite.flip_h = true
+	elif direction.x < -0.1:
+		sprite.flip_h = false
--- Player/player.gd
+++ Player/player.gd
@@ -2,6 +2,9 @@ extends CharacterBody2D
 
 
 var movement_speed = 40.0 #basic speed added 
+var hp = 80
+@onready var sprite: Sprite2D = $Sprite2D
+@onready var walkTimer: Timer = %walkTimer
 
 func _physics_process(delta: float) -> void:#runs on every 60th of the frame i.e 1/60 sec
 	movement() #custom function 
@@ -15,6 +18,19 @@ func movement(): #function determining the movement direction of the player
 	#y_mov =0-0=0
 	#defining mov variable to place the character's poisiton on the map using theabove defined functions
 	var mov = Vector2(x_mov, y_mov) #mov = Vector2(1x,-y)
+	if mov.x > 0:
+		sprite.flip_h = true
+	elif mov.x<0:
+		sprite.flip_h = false
+	
+	if mov != Vector2.ZERO: #Vector2(0,0)
+		if walkTimer.is_stopped():
+			if sprite.frame >= sprite.hframes-1:
+				sprite.frame = 0
+			else:
+				sprite.frame+=1
+			walkTimer.start()
+		
 	velocity = mov.normalized()*movement_speed #to make the body move we apply velocity to it 
 	#so assuming D is pressed velocity = Vector2(1x,0y)&40 = Vector2(40x, 0y)
 	#this means the body rn is moving currently  in +40 pixel/sec with 0 y
@@ -22,3 +38,8 @@ func movement(): #function determining the movement direction of the player
 	move_and_slide()
 	
 	#
+
+
+func _on_hurt_box_hurt(damage: Variant) -> void:
+	hp-=damage
+	print(hp)
