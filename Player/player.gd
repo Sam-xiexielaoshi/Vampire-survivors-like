103f12a4b58ffefa1c7d984220bb2a32e60b4bfe
Sam-xiexielaoshi <samagrasingh20032003@gmail.com>
2025-08-06 01:04:48 +0530

    added icespear to the player attack withj enemy detection system for guiding of icicles

--- Player/player.gd
+++ Player/player.gd
@@ -6,6 +6,25 @@ var hp = 80
 @onready var sprite: Sprite2D = $Sprite2D
 @onready var walkTimer: Timer = %walkTimer
 
+#attack
+var iceSpear = preload("res://Player/Attack/ice_spear.tscn")
+
+#aatack nodes
+@onready var iceSpearTimer = get_node("%IceSpearTimer")
+@onready var iceSpearAttackTimer = get_node("%IceSpearAttackTimer")
+
+#Ice spear
+var icespeear_ammo = 0
+var icespear_baseammo =1
+var icespear_attackspeed = 1.5
+var icespear_level =1
+
+#Enemy related
+var enemy_close = []
+
+func _ready() -> void:
+	attack()
+
 func _physics_process(delta: float) -> void:#runs on every 60th of the frame i.e 1/60 sec
 	movement() #custom function 
 	
@@ -36,6 +55,48 @@ func movement(): #function determining the movement direction of the player
 	#this means the body rn is moving currently  in +40 pixel/sec with 0 y
 	#cause the character to movea
 	move_and_slide()
+
+func attack():
+	if icespear_level>0:
+		iceSpearTimer.wait_time = icespear_attackspeed
+		if iceSpearTimer.is_stopped():
+			iceSpearTimer.start()
+
 func _on_hurt_box_hurt(damage: Variant) -> void:
 	hp-=damage
 	print(hp)
+
+
+func _on_ice_spear_timer_timeout() -> void:
+	icespeear_ammo += icespear_baseammo
+	iceSpearAttackTimer.start()
+
+
+func _on_ice_spear_attack_timer_timeout() -> void:
+	if icespeear_ammo > 0:
+		var icespear_attack = iceSpear.instantiate()
+		icespear_attack.position = position
+		icespear_attack.target = get_random_target()
+		icespear_attack.level = icespear_level
+		add_child(icespear_attack)
+		icespeear_ammo -= 1
+		if icespeear_ammo >0:
+			iceSpearAttackTimer.start()
+		else:
+			iceSpearAttackTimer.stop()
+		
+func get_random_target():
+	if enemy_close.size()>0:
+		return enemy_close.pick_random().global_position
+	else:
+		return Vector2.UP
+
+
+func _on_enemy_detection_area_body_entered(body: Node2D) -> void:
+	if not enemy_close.has(body):
+		enemy_close.append(body)
+
+
+func _on_enemy_detection_area_body_exited(body: Node2D) -> void:
+	if enemy_close.has(body):
+		enemy_close.erase(body)
